4d8ba4dcaf5444a6402a0b9ff050f9e7dedfe147
ssalabura <stagerek@gmail.com>
2021-04-01 21:23:07 +0200

    proper bottom navigation bar

--- app/src/main/java/com/ssalabura/meltee/ui/addphoto/AddPhotoFragment.java
+++ app/src/main/java/com/ssalabura/meltee/ui/addphoto/AddPhotoFragment.java
@@ -1,4 +1,4 @@
-package com.ssalabura.meltee.ui.notifications;
+package com.ssalabura.meltee.ui.addphoto;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -14,17 +14,17 @@ import androidx.lifecycle.ViewModelProvider;
 
 import com.ssalabura.meltee.R;
 
-public class NotificationsFragment extends Fragment {
+public class AddPhotoFragment extends Fragment {
 
-    private NotificationsViewModel notificationsViewModel;
+    private AddPhotoViewModel homeViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        notificationsViewModel =
-                new ViewModelProvider(this).get(NotificationsViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_notifications, container, false);
-        final TextView textView = root.findViewById(R.id.text_notifications);
-        notificationsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+        homeViewModel =
+                new ViewModelProvider(this).get(AddPhotoViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_add_photo, container, false);
+        final TextView textView = root.findViewById(R.id.text_home);
+        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
--- app/src/main/java/com/ssalabura/meltee/ui/addphoto/AddPhotoViewModel.java
+++ app/src/main/java/com/ssalabura/meltee/ui/addphoto/AddPhotoViewModel.java
@@ -1,16 +1,16 @@
-package com.ssalabura.meltee.ui.notifications;
+package com.ssalabura.meltee.ui.addphoto;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class NotificationsViewModel extends ViewModel {
+public class AddPhotoViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
 
-    public NotificationsViewModel() {
+    public AddPhotoViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is notifications fragment");
+        mText.setValue("This is add_photo fragment");
     }
 
     public LiveData<String> getText() {
