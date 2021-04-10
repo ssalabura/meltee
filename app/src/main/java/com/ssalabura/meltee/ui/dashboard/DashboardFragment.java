979af437e873136b3ea8f01c4b3ba257e9ad01e7
ssalabura <stagerek@gmail.com>
2021-04-10 23:42:38 +0200

    changed layout in dashboard to SwipeRefreshLayout

--- app/src/main/java/com/ssalabura/meltee/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/ssalabura/meltee/ui/dashboard/DashboardFragment.java
@@ -4,11 +4,13 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import com.ssalabura.meltee.R;
 
@@ -17,28 +19,41 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class DashboardFragment extends Fragment {
+    SwipeRefreshLayout swipeRefreshLayout;
+    RecyclerView recyclerView;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_dashboard, container, false);
 
-        List<PhotoCard> photoCardList = getPhotoCards();
-        RecyclerView recyclerView = root.findViewById(R.id.recyclerView);
-        if(photoCardList.size() > 0) {
-            root.findViewById(R.id.textView_empty).setVisibility(View.INVISIBLE);
-        }
-        recyclerView.setAdapter(new RecyclerViewAdapter(getContext(), photoCardList));
+        recyclerView = root.findViewById(R.id.recyclerView);
+
+        swipeRefreshLayout = root.findViewById(R.id.swipeRefresh);
+        swipeRefreshLayout.setOnRefreshListener(() -> {
+            //TODO: read from online database and write to local
+            refreshPhotoCards(root);
+        });
+
+        refreshPhotoCards(root);
 
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false);
         recyclerView.setLayoutManager(linearLayoutManager);
         return root;
     }
 
-    private List<PhotoCard> getPhotoCards() {
-        List<PhotoCard> photoCards = new ArrayList<>();
+    private void refreshPhotoCards(View root) {
+        //TODO: read from device database
+        List<PhotoCard> photoCardList = new ArrayList<>();
         for(File f : getContext().getExternalMediaDirs()[0].listFiles()) {
-            photoCards.add(new PhotoCard(f.getAbsolutePath(), f.getName()));
+            photoCardList.add(new PhotoCard(f.getAbsolutePath(), f.getName()));
         }
-        return photoCards;
+        TextView empty = root.findViewById(R.id.textView_empty);
+        if(photoCardList.size() > 0) {
+            empty.setVisibility(View.INVISIBLE);
+        } else {
+            empty.setVisibility(View.VISIBLE);
+        }
+        recyclerView.setAdapter(new RecyclerViewAdapter(getContext(), photoCardList));
+        swipeRefreshLayout.setRefreshing(false);
     }
 }
